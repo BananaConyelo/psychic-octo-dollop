ebd7f87b0afeae4e5bbe307b46156248f10b7200
Jessie <133477439+BananaConyelo@users.noreply.github.com>
2024-11-18 23:03:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('user-icon').addEventListener('click', function() {
+    document.getElementById('login-form').style.display = 'block';
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('login-form').style.display = 'none';
+});
